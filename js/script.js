2507b7a20efdf5f37d9de4561118895e7db413e2
Arturo Mora-Rioja <arturomora@gmail.com>
2025-01-13 10:58:51 +0100

    Refactoring. Semantic HTML and accessibility improved

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 /**
  * Clicking on the "Move selected text" button moves the selected text to the results div
  */
-document.querySelector('#btnMove').addEventListener('click', (e) => {
+document.querySelector('#frmMoveText').addEventListener('submit', (e) => {
     e.preventDefault();
 
     const radioId = document.querySelector('input[type="radio"][name="radText"]:checked').id;
@@ -19,8 +19,13 @@ document.querySelector('#cmbColours').addEventListener('change', () => {
     // const newColour = document.querySelectorAll('option')[document.querySelector('#cmbColours').selectedIndex].value;
 
     // Approach #2. As it is a comboBox, not a listBox, there is only one selected option
-    const newColour = document.querySelector('#cmbColours').selectedOptions[0].value;
+    // const newColour = document.querySelector('#cmbColours').selectedOptions[0].value;
 
+    // Approach #3 (better). The value of the comboBox is the value of the selected option
+    const newColour = document.querySelector('#cmbColours').value;
+
+    // The following line is convenient because the value of the selected option is the CSS name of the colour.
+    // However, it threatens the separation of concerns principle by addressing CSS in JavaScript
     document.querySelector('#results').style.backgroundColor = newColour;
 });
 
@@ -28,21 +33,23 @@ document.querySelector('#cmbColours').addEventListener('change', () => {
  * Clicking on each of the texts will select the corresponding radio button
  */
 
-// Approach #1
+// Approach #1. The event listener code is written directly in the event listener.
+// Simple, easy to read code for small event listeners
 
-// document.querySelectorAll('section#texts > article > p').forEach((paragraph) => {
+// document.querySelectorAll('#texts > article a.text').forEach((paragraph) => {
 //     paragraph.addEventListener('click', () => {
 //         const paragraphId = paragraph.id;
 //         document.querySelector('#radText' + paragraphId.substring(paragraphId.length - 1)).click();
 //     });
 // });
 
-// Approach #2
+// Approach #2. The event listener code is written in a function.
+// It aids code modularity. Preferred approach for event listeners with large code blocks
 
 const handleParagraphClick = function() {   // It cannot be an arrow function, because "this" would not be accessible
     const paragraphId = this.id;
     document.querySelector('#radText' + paragraphId.substring(paragraphId.length - 1)).click();
 }
-document.querySelectorAll('section#texts > article > p').forEach((paragraph) => {
+document.querySelectorAll('#texts > article a.text').forEach((paragraph) => {
     paragraph.addEventListener('click', handleParagraphClick);
 });
